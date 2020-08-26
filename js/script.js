a8e899b3813ea1bbbd77e2c80d43293598619b0e
Lasse Hindsberg <3404293@rts-365.dk>
2020-08-26 08:35:27 +0200

    Final Commit

--- js/script.js
+++ js/script.js
@@ -1,13 +1,21 @@
 let form = document.getElementsByTagName("form")[0];
 let inputFields = document.querySelectorAll("input");
+let success = true;
 
-form.addEventListener("submit", function(event){
+form.addEventListener("submit", function (event) {
     event.preventDefault();
 
-    inputFields.forEach(function(input) {
-        if (input.hasAttribute("required")) (&& input.value == "") {
-            input.style.backgroundColor = "red";
-            
-        } 
-        });
-    })
+    inputFields.forEach(function (input) {
+        input.style.backgroundColor = "inherit";
+        form.querySelector(".statusMessage." + input.name).innerText = "";
+        if (input.hasAttribute("required") && (input.value == "")) {
+            input.style.backgroundColor = "darkRed";
+            // input.style.color = "#03090D";
+            form.querySelector(".statusMessage." + input.name).innerText = "Udfyld dette felt.";
+            success = false;
+        }
+    });
+
+    if (!success) return;
+    fetch(login.php);
+});
